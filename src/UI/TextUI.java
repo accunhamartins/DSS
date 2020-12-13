61bc220d0f2aff4ff66b3ec1d3cc105d39ed9d9c
141Hash <accmartins3000@gmail.com>
2020-12-13 18:14:10 +0000

    Stuff

--- src/UI/TextUI.java
+++ src/UI/TextUI.java
@@ -1,4 +1,22 @@
 package UI;
 
+import java.util.Scanner;
+
 public class TextUI {
+
+    private Menu menu;
+
+    private Scanner scanner;
+
+
+    public TextUI() {
+        String[] opcoes = {
+                "Comunicar Código QR",
+                "Notificar recolha de palete",
+                "Notificar entrega de palete",
+                "Consultar listagem de localização"};
+        this.menu = new Menu(opcoes);
+        this.scanner = new Scanner(System.in);
+    }
 }
+
--- src/business/Localizacao.java
+++ src/business/Localizacao.java
@@ -3,4 +3,25 @@ package business;
 public class Localizacao {
     private int corredor;
     private int prateleira;
+
+    public Localizacao(){
+        this.corredor = 0;
+        this.prateleira = 0;
+    }
+
+    public int getCorredor() {
+        return this.corredor;
+    }
+
+    public int getPrateleira() {
+        return this.prateleira;
+    }
+
+    public void setCorredor(int corredor) {
+        this.corredor = this.corredor;
+    }
+
+    public void setPrateleira(int prateleira) {
+        this.prateleira = this.prateleira;
+    }
 }
