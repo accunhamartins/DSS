153e5f642256e830cd8f9c84ce28108ce498ae83
André Martins <accmartins3000@gmail.com>
2020-12-21 23:50:05 +0000

    Timezone

--- src/data/DAOConfig.java
+++ src/data/DAOConfig.java
@@ -5,5 +5,5 @@ public class DAOConfig {
         static final String PASSWORD = "root";
         private static final String DATABASE = "Armazem";
         private static final String DRIVER = "jdbc:mysql";
-        static final String URL = DRIVER+"://localhost:3306/"+DATABASE;
+        static final String URL = DRIVER+"://localhost:3306/"+DATABASE+"?useTimezone=true&serverTimezone=GMT";
 }
